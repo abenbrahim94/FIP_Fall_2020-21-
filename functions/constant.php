47c7b35673d6cac12232030494a360322b9c95cf
abenbrahim94 <abenbrahim94@gmail.com>
2020-12-08 13:58:07 -0500

    Update constant.php

--- functions/constant.php
+++ functions/constant.php
@@ -1,6 +1,6 @@
 <?php
 define("DB_HOST","localhost");
-define("DB_USER","root");
-define("DB_PASS","");
-define("DB_NAME","test");
+define("DB_USER","AhmedBenbrahim");
+define("DB_PASS","*******");
+define("DB_NAME","database");
 ?>
